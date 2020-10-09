b3d08425634599eda4460ff89eb46cf609ad23b3
VivaEcha838 <68964649+VivaEcha838@users.noreply.github.com>
2020-10-08 19:21:06 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+//Create variables here
+
+var dog, doggy, happyDog, database, foodS, foodStock, uA;
+
+
+function preload()
+{
+  //load images here
+  dog = loadAnimation("images/dogImg1.png");
+  happyDog = loadAnimation("images/dogImg.png");
+}
+
+function setup() {
+  createCanvas(500,500);
+  database = firebase.database();
+
+  doggy = createSprite(200,200,20,20);
+  doggy.addAnimation("dog",dog);
+  doggy.addAnimation("happyDog", happyDog);
+
+  doggy.scale = 0.1; 
+
+  foodStock = database.ref('Food');
+ foodStock.on("value", readStock);
+
+  
+}
+
+
+function draw() {  
+  background(46,139,87);
+
+
+
+
+  if(keyDown(UP_ARROW)){
+    writeStock(foodS);
+    doggy.changeAnimation("happyDog",happyDog);
+    doggy.scale = 0.1;
+
+   
+  }
+  textSize(16);
+  fill("red");
+stroke("green");
+  text("Food Remaining: " + foodS, 250,125);
+  
+
+  drawSprites();
+  //add styles here
+  
+
+}
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+if(x<=0){
+  x=0;
+}else{
+  x=x-1; 
+}
+
+  database.ref('/').update({
+    Food : x
+  })
+}
+
+
+
